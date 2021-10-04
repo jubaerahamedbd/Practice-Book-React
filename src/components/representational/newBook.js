d530ee52f1d259fe43e9ef14d445c23ed699b597
jubaerahamedbd <jubaerahamed@gmail.com>
2021-10-04 20:48:33 +0600

    Uncontrolled Form

--- src/components/representational/newBook.js
+++ src/components/representational/newBook.js
@@ -1,29 +1,21 @@
-import React, { Component } from 'react'
+import React, { Component, createRef } from 'react'
 
 class NewBook extends Component {
     constructor(props) {
         super(props)
-        this.state = {
-            bookName: "",
-            writer: "",
-            description: ""
-        }
-        this.handleInputChange = this.handleInputChange.bind(this)
+        this.bookName = createRef();
+        this.writer = createRef();
+        this.description = createRef();
         this.handleSubmit = this.handleSubmit.bind(this)
     }
 
     handleSubmit = (event) => {
-        console.log(this.state)
+        console.log(this.bookName.current.value)
+        console.log(this.writer.current.value)
+        console.log(this.description.current.value)
         event.preventDefault();
     }
 
-    handleInputChange = (event) => {
-        const name = event.target.name;
-        const value = event.target.value;
-        this.setState({
-            [name]: value
-        })
-    }
 
     render() {
         return (
@@ -31,11 +23,11 @@ class NewBook extends Component {
                 <h1>New Book Entry</h1>
                 <form onSubmit={this.handleSubmit}>
                     <label>Book Name:</label><br />
-                    <input type="text" name="bookName" value={this.state.bookName} onChange={this.handleInputChange} /><br />
+                    <input type="text" name="bookName" ref={this.bookName} /><br />
                     <label>Writer:</label><br />
-                    <input type="text" name="writer" value={this.state.writer} onChange={this.handleInputChange} /><br />
+                    <input type="text" name="writer" ref={this.writer} /><br />
                     <label>Description:</label><br />
-                    <textarea type="text" name="description" value={this.state.description} onChange={this.handleInputChange} /><br />
+                    <textarea type="text" name="description" ref={this.description} /><br />
                     <input type="submit" value="Submit" />
                 </form>
             </div>
